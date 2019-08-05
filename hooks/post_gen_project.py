0c96cc7036b9225fca92dd967910b229cd6b856a
Flavio Grandin <flavio.grandin@gmail.com>
2019-08-05 20:27:29 +0200

    pylintrc file generated

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,18 @@
 import os.path
 
 TEX_DIR = 'tex'
-TEMPLATE = r'''\documentclass[preview,border=0.5cm]{standalone}
-\begin{document}
+TEMPLATE = '''
+\\documentclass[preview,border=0.5cm]{standalone}
+\\usepackage[utf8]{inputenc}
+\\usepackage[italian]{babel}
+\\begin{document}
 
-\end{document}
-'''
+\\end{document}
+'''.strip()
 
-n = {{ cookiecutter.number_of_problems }}
+NUMBER_OF_PROBLEMS = {{ cookiecutter.number_of_problems }}
 
-for i in range(n):
+for i in range(NUMBER_OF_PROBLEMS):
 	problem = os.path.join(TEX_DIR, f'problem{i+1}.tex')
 	solution = os.path.join(TEX_DIR, f'solution{i+1}.tex')
 	with open(problem, 'w') as f:
